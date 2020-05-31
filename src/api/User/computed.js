edbd78d06168b65c8650628b01fc8558643f14b3
songjein <jeinsong100@gmail.com>
2020-05-31 19:03:09 +0900

    isSelf, isFollowing computed field

---
+++ src/api/User/computed.js
@@ -0,0 +1,27 @@
+import { prisma } from "../../../generated/prisma-client";
+
+export default {
+  User: {
+    // fragment에선 접근 못하는듯!
+    fullName: parent => {
+      return `${parent.firstName} ${parent.lastName}`;
+    },
+    isFollowing: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      try {
+        return prisma.$exists.user({
+          AND: [{ id: parentId }, { followers_some: { id: user.id } }]
+          // AND: [{ id: user.id }, { following_some: { id: parentId } }]
+        });
+      } catch {
+        return false;
+      }
+    },
+    isSelf: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      return user.id === parentId;
+    }
+  }
+};
